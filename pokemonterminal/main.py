3e05bace4fe93029121e010aac6ae836d9b3f0a6
Jack Hopper <jackhopper17@gmail.com>
2025-05-15 21:57:58 +0100

    Show image path on verbose

--- pokemonterminal/main.py
+++ pokemonterminal/main.py
@@ -62,7 +62,7 @@ def main(argv=None):
                 print(f"#{pkmn.get_id()} - {pkmn.get_name().title()}")
                 for pkmn in Filter.filtered_list
             ]
-        print("Total of %d pokemon matched the filters. Chose %s." %
+        print("Total of %d pokemon matched the filters. Chose %s" %
               (size, target.get_name().title()))
         print("Path: %s" % target.get_path())
 
