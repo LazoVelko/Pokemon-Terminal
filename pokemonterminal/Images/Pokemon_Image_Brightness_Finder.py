7ff426115ef664ff4f7a1751bb5cc90aa8d30e3a
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-06-05 13:15:56 +0100

    Updated some file directories

--- pokemonterminal/Images/Pokemon_Image_Brightness_Finder.py
+++ pokemonterminal/Images/Pokemon_Image_Brightness_Finder.py
@@ -2,15 +2,15 @@ import cv2
 import os
 
 def pokemonImageBrightnessFinder(fileName):
+    image = cv2.imread('/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images/' + fileName)
     # image = cv2.imread('/home/adam/Pokemon_Images/Generation VI - Kalos/0655Delphox.png')
-    image = cv2.imread('/home/mark/Documents/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/Generation VI - Kalos/' + fileName)
     grayscaleImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     meanBrightness = grayscaleImage.mean()
     meanBrightness = meanBrightness / 255
     meanBrightness = round(meanBrightness, 3) 
     print(meanBrightness)
 
+for fileName in os.listdir('/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images'):
 # for fileName in os.listdir('/home/adam/Pokemon_Images/Generation VI - Kalos'):
-for fileName in os.listdir('/home/mark/Documents/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/Generation VI - Kalos'):
-    if fileName.endswith('.jpg'):
+    if fileName.endswith('.png'):
         pokemonImageBrightnessFinder(fileName)
