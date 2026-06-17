adb48315f308e77368a91f9fe94181bdf4bd111e
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-06-17 14:22:44 +0100

    Delete pokemonterminal/Images/Pokemon_API_Test.py

--- pokemonterminal/Images/Pokemon_API_Test.py
+++
@@ -1,9 +0,0 @@
-import pypokedex
-pokemonTypes = pypokedex.get(dex=655)
-
-
-multipleTypes = ''
-for type in pokemonTypes.types:
-    multipleTypes = multipleTypes + type + '\t'
-
-print(multipleTypes)
