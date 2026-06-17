cd91f7e00211105e284ccb23546b69898120a5e1
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-06-17 11:13:04 +0100

    Updated the image brightness finder.
    Had this superior version for a while but never pushed it. Working on pushing my entire folder to Github so I'm making sure I'm not losing anything.

--- pokemonterminal/Images/Pokemon_Image_Brightness_Finder.py
+++ pokemonterminal/Images/Pokemon_Image_Brightness_Finder.py
@@ -1,19 +1,21 @@
 import cv2
 import os
 
-folderPath = '/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images'
-# folderPath = '/home/adam/Pokemon_Images/Pokemon/assets/imagesHQ/'
+# folderPath = '/home/mark/Documents/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/Test'
+folderPath = '/home/adam/Pokemon_Images/Pokemon/assets/HQ_Images/'
 
 def pokemonImageBrightnessFinder(fileName):
+    # image = cv2.imread('/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images/' + fileName)
     image = cv2.imread(folderPath + fileName)   
     grayscaleImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     meanBrightness = grayscaleImage.mean()
     meanBrightness = meanBrightness / 255
     meanBrightness = round(meanBrightness, 3)
-    print (meanBrightness)
+    return meanBrightness
+    # print(meanBrightness)
 
-pokemonImageBrightnessFinder('0655')
-# for fileName in os.listdir('/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images'):
-for imageNameInput in os.listdir(folderPath):
-    if imageNameInput.endswith('.png'):
-        pokemonImageBrightnessFinder(imageNameInput)
+# pokemonImageBrightnessFinder('1032_Gecqua.png')
+
+# for pkmnID in os.listdir(folderPath):
+#     if pkmnID.endswith('.png'):
+#         pokemonImageBrightnessFinder(pkmnID)
