52f66c8ae86ddbacb536a8b3e373b80476f9b593
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-06-22 12:04:13 +0100

    Testing a way to ignore blank space

--- pokemonterminal/Images/Pokemon_Image_Mean_Colour_Finder.py
+++ pokemonterminal/Images/Pokemon_Image_Mean_Colour_Finder.py
@@ -10,28 +10,36 @@ list.sort()
 
 def pokemonImageMeanColourFinder(fileName):
     pokemonDexNo = fileName[:4]
-    pkmnImage = cv2.imread(folderPath + fileName)   
-    averageColourRow = np.average(pkmnImage, axis=0)
-    averageColour= np.average(averageColourRow, axis=0)
-    averageRedColourValue = int(averageColour[0])
-    averageGreenColourValue = int(averageColour[1])
-    averageBlueColourValue = int(averageColour[2])
+    pkmnImage = cv2.imread(folderPath + fileName)
+    rows,cols,_ = pkmnImage.shape
+    for i in range(rows):
+        for j in range(cols):
+            k = pkmnImage[i,j]
+            print(k)
+
+    # averageColourRow = np.average(pkmnImage, axis=0)
+    # averageColour= np.average(averageColourRow, axis=0)
+    # averageRedColourValue = int(averageColour[0])
+    # averageGreenColourValue = int(averageColour[1])
+    # averageBlueColourValue = int(averageColour[2])
+    
+    # # print(pokemonDexNo)
+    # # print(averageRedColourValue)
+    # # print(averageGreenColourValue)
+    # # print(averageBlueColourValue)
     
     # print(pokemonDexNo)
-    # print(averageRedColourValue)
-    # print(averageGreenColourValue)
-    # print(averageBlueColourValue)
-    
-    print(averageColour)
 
-    print (averageRedColourValue, averageGreenColourValue, averageBlueColourValue)
+    # print (averageRedColourValue, averageGreenColourValue, averageBlueColourValue)
     
     
 
-pokemonImageMeanColourFinder('0014_Kakuna.png')
+pokemonImageMeanColourFinder('0011_Metapod.png')
 
 # for pkmnID in list:
 #     if pkmnID.endswith('.png'):
 #         pokemonImageBrightnessFinder(pkmnID)
 
-# https://stackoverflow.com/questions/55292321/read-png-image-with-cv2-imread-form-opencv3-in-python3-7-1-and-no-pixel-at-all-w
+
+
+
