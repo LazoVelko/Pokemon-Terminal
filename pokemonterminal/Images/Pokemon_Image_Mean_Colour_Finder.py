5d18d8a0d15be8f3209f08a0fc01ec016bab3fc7
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-06-22 16:19:36 +0100

    Troubleshooting some issues with transparent pixels messing with the mean colour calculations.

--- pokemonterminal/Images/Pokemon_Image_Mean_Colour_Finder.py
+++ pokemonterminal/Images/Pokemon_Image_Mean_Colour_Finder.py
@@ -1,4 +1,32 @@
 import cv2
+import numpy as np
+from PIL import Image
+
+# folderPath = '/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images/'
+folderPath = '/home/adam/Pokemon_Images/Pokemon/assets/'
+
+
+list = os.listdir(folderPath)
+list.sort()
+
+def pokemonImageMeanColourFinder(fileName):
+    pokemonDexNo = fileName[:4]
+    pkmnImage = cv2.imread(folderPath + fileName)   
+    averageColourRow = np.average(pkmnImage, axis=0)
+    averageColour= np.average(averageColourRow, axis=0)
+    averageRedColourValue = int(averageColour[0])
+    averageGreenColourValue = int(averageColour[1])
+    averageBlueColourValue = int(averageColour[2])
+    
+    print(pokemonDexNo)
+    print(averageRedColourValue)
+    print(averageGreenColourValue)
+    print(averageBlueColourValue)
+    
+
+pokemonImageMeanColourFinder('test.png')
+
+import cv2
 import os
 import numpy as np
 
