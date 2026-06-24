6c3e785ac619f5c963f6de7d88c544fae5c2fb96
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-06-24 11:58:14 +0100

    I'm really struggling with ignoring transparent pixels...

--- pokemonterminal/Images/Pokemon_Image_Mean_Colour_Finder.py
+++ pokemonterminal/Images/Pokemon_Image_Mean_Colour_Finder.py
@@ -1,73 +1,54 @@
+import os
 import cv2
 import numpy as np
 from PIL import Image
 
 # folderPath = '/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images/'
-folderPath = '/home/adam/Pokemon_Images/Pokemon/assets/'
+folderPath = '/home/adam/Pokemon_Images/Pokemon/assets/HQ_Images/'
+fileID = '0655_Delphox-Mega.png'
 
 
-list = os.listdir(folderPath)
-list.sort()
+# list = os.listdir(folderPath)
+# list.sort()
 
-def pokemonImageMeanColourFinder(fileName):
-    pokemonDexNo = fileName[:4]
-    pkmnImage = cv2.imread(folderPath + fileName)   
-    averageColourRow = np.average(pkmnImage, axis=0)
-    averageColour= np.average(averageColourRow, axis=0)
-    averageRedColourValue = int(averageColour[0])
-    averageGreenColourValue = int(averageColour[1])
-    averageBlueColourValue = int(averageColour[2])
-    
-    print(pokemonDexNo)
-    print(averageRedColourValue)
-    print(averageGreenColourValue)
-    print(averageBlueColourValue)
+# def pokemonImageMeanColourFinder(fileID):
+#     pokemonDexNo = fileID[:4]
+#     pkmnImage = cv2.imread(f'imgs/{folderPath + fileID}')
     
 
-pokemonImageMeanColourFinder('test.png')
 
-import cv2
-import os
-import numpy as np
+#     averageColourRow = np.average(pkmnImage, axis=0)
+#     averageColour= np.average(averageColourRow, axis=0)
 
-folderPath = '/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images/'
-# folderPath = '/home/adam/Pokemon_Images/Pokemon/assets/HQ_Images/'
 
-list = os.listdir(folderPath)
-list.sort()
+#     averageRedColourValue = int(averageColour[0])
+#     averageGreenColourValue = int(averageColour[1])
+#     averageBlueColourValue = int(averageColour[2])
 
-def pokemonImageMeanColourFinder(fileName):
-    pokemonDexNo = fileName[:4]
-    pkmnImage = cv2.imread(folderPath + fileName)
-    rows,cols,_ = pkmnImage.shape
-    for i in range(rows):
-        for j in range(cols):
-            k = pkmnImage[i,j]
-            print(k)
 
-    # averageColourRow = np.average(pkmnImage, axis=0)
-    # averageColour= np.average(averageColourRow, axis=0)
-    # averageRedColourValue = int(averageColour[0])
-    # averageGreenColourValue = int(averageColour[1])
-    # averageBlueColourValue = int(averageColour[2])
-    
-    # # print(pokemonDexNo)
-    # # print(averageRedColourValue)
-    # # print(averageGreenColourValue)
-    # # print(averageBlueColourValue)
-    
-    # print(pokemonDexNo)
-
-    # print (averageRedColourValue, averageGreenColourValue, averageBlueColourValue)
-    
+#     print(pokemonDexNo)
+#     print(averageRedColourValue)
+#     print(averageGreenColourValue)
+#     print(averageBlueColourValue)
     
 
-pokemonImageMeanColourFinder('0011_Metapod.png')
-
-# for pkmnID in list:
-#     if pkmnID.endswith('.png'):
-#         pokemonImageBrightnessFinder(pkmnID)
+# for fileName in os.listdir(folderPath):
+#     if fileName.endswith('.png'):
+#         pokemonImageMeanColourFinder(fileName)
 
 
+pkmnImage = Image.open('/home/adam/Pokemon_Images/Pokemon/assets/HQ_Images/0655_Delphox-Mega.png')
+
+pixels = pkmnImage.load()
+width, height = pkmnImage.size
+
+for x in range(width):
+    for y in range(height):
+            r, g, b, a = pixels[x, y]
+
+print(r)
 
 
+# print(pkmnImage.shape)
+
+# https://pytutorial.com/python-get-image-pixels-guide/
