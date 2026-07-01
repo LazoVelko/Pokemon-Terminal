0ba1262309ab7ff515365906fbaa32951428792f
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-07-01 10:28:29 +0100

    Made an accurate average brightness finder.
    This script can open an image, convert it into grayscale and alpha channels, and then read its data.
    
    It look through every pixel of an image using a loop, any pixels with 0 alpha will be ignored, and every other pixel will have its brightness appended into a list.
    
    At the end of the loop, all values in the list are added together, then divided by the length of the list to find the average, then divided by 255 so that the output is between 0 and 1, and then it's rounded to the 3rd decimal place to look neater.

--- pokemonterminal/Images/Pokemon_Image_Brightness_Finder.py
+++ pokemonterminal/Images/Pokemon_Image_Brightness_Finder.py
@@ -1,44 +1,45 @@
 import os
-
 import numpy as np
 from PIL import Image
 
+# folderPath = '/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images/'
+folderPath = '/home/adam/Pokemon_Images/Pokemon/assets/HQ_Images/'
 
-# folderPath = '/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images'
-folderPath = '/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images/'
-grayFolderPath = '/home/mark/Adams_Dev_Test/Pokemon-Terminal/pokemonterminal/Images/HQ_Images_Gray/'
-# folderPath = '/home/adam/Pokemon_Images/Pokemon/assets/HQ_Images/'
-# grayFolderPath = '/home/adam/Pokemon_Images/Pokemon/assets/Gray_HQ_Images/'
-
-def pokemonImageBrightnessFinder(imageInput, imageOutput, fileName):
+def pokemonImageBrightnessFinder(imageInput, fileName):
     fileInput = (imageInput + fileName)
-    fileOutput = (imageOutput + fileName)
-
-    # pkmnImg = Image.open(fileInput)
-    # pkmnImgRGBA = pkmnImg.convert('RGBA')
-    # pkmnImgData = pkmnImgRGBA.getdata()
+    # fileOutput = (imageOutput + fileName)
 
     pkmnImg = Image.open(fileInput)
-    pkmnImgGray = pkmnImg.convert('L')
-    pkmnImgGray.save(fileOutput)
-
-    pkmnImgGrayNew = Image.open(fileOutput)
-    pkmnImgRGBA = pkmnImgGrayNew.convert('RGBA')
+    pkmnImgRGBA = pkmnImg.convert('LA')
     pkmnImgData = pkmnImgRGBA.getdata()
 
+        # Obsolete script, doesn't work how I want it to,
+        # causes image artifacting on PNGs, which taints
+        # the average brightness of the Pokémon.
+
+    # pkmnImg = Image.open(fileInput)
+    # pkmnImgGray = pkmnImg.convert('L')
+    # pkmnImgGray.save(fileOutput)
+
+    # pkmnImgGrayNew = Image.open(fileOutput)
+    # pkmnImgRGBA = pkmnImgGrayNew.convert('RGBA')
+    # pkmnImgData = pkmnImgRGBA.getdata()
+
     meanBrightness = []
+    meanBrightnessList = []
     
     for pixelValue in pkmnImgData:
-        if pixelValue[3] == 0:
+        if pixelValue[1] == 0:
             continue
         else:
-            meanBrightness = int(sum(pixelValue)) / int(len(pixelValue))
-            print(meanBrightness)
+            meanBrightnessList.append(int(sum(pixelValue[:1])))
 
+    meanBrightness = round((int(sum(meanBrightnessList)) / int(len(meanBrightnessList)) / 255), 3)
+    print(meanBrightness)
 
+# for pkmnID in os.listdir(folderPath):
+#     if pkmnID.endswith('.png'):
+#         pokemonImageBrightnessFinder(folderPath, grayFolderPath, '0014_Kakuna.png')
 
-for pkmnID in os.listdir(folderPath):
-    if pkmnID.endswith('.png'):
-        pokemonImageBrightnessFinder(folderPath, grayFolderPath, '0014_Kakuna.png')
+pokemonImageBrightnessFinder(folderPath, '1032_Gecqua.png')
 
-# https://pythonexamples.org/pillow-convert-image-to-grayscale/
