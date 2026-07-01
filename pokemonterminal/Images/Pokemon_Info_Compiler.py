2e4b76c1e9f224de1c900b8c19735172f168e39b
MegaDelphoxEnjoyer <AdamKaanGambrill123@outlook.com>
2026-07-01 12:05:52 +0100

    Added a way to read and ignore variants with wrong types.

--- pokemonterminal/Images/Pokemon_Info_Compiler.py
+++ pokemonterminal/Images/Pokemon_Info_Compiler.py
@@ -7,28 +7,36 @@ folderPath = '/home/adam/Pokemon_Images/Pokemon/assets/HQ_Images/'
 
 list = os.listdir(folderPath)
 list.sort()
+
+open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Temp.txt', 'w').close()
+open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Log_Temp.txt', 'w').close()
+
+pkmnTypeVariants = open('/home/adam/Pokemon_Images/Adams_Tests/Pokemon_Mega_And_Regional_Type_Variations.py').read().split()
+pkmnNameLog = open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Log.txt').read().split()
+
 testCount = 0
 
-open ('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Temp.txt', 'w').close()
-open ('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Log.txt', 'w').close()
-
 for fileName in list:
-    if fileName in open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Log.txt').readlines():
+    if fileName in pkmnNameLog:
         continue
-    pokemonDexNo = fileName[:4]
-    pokemonDexNo = int(pokemonDexNo)
-    pokemonDexNoFixed = ("{:04d}".format(pokemonDexNo))
-    pokemonName = fileName[5:-4]
-    pokemonVariable = pypokedex.get(dex=pokemonDexNo)
-    # pokemonTypes = ['Fire', 'Psychic']
-    testCount = testCount + 1
-    print(testCount)
-    multipleTypes = ''
+    elif fileName in pkmnTypeVariants:
+        testCount = testCount + 1
+        print(testCount)
+        print(fileName)
+        continue
+    else:
+        pokemonDexNo = fileName[:4]
+        pokemonDexNo = int(pokemonDexNo)
+        pokemonDexNoFixed = ("{:04d}".format(pokemonDexNo))
+        pokemonName = fileName[5:-4]
+        pokemonVariable = pypokedex.get(dex=pokemonDexNo)
+        # pokemonTypes = ['Fire', 'Psychic']
+        multipleTypes = ''
 
     for pokeType in pokemonVariable.types:
         multipleTypes = multipleTypes + '\t' + pokeType
     # with open('/home/mark/Adams_Dev_Test/Pokemon_Compiled_Info.txt', 'a') as compiledTxt:
-    with open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info.txt', 'a') as compiledCsv:
+    with open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Temp.txt', 'a') as compiledCsv:
         compiledCsv.write(str(pokemonDexNoFixed) + '\t' + pokemonName.title() + '\t' + str(imageBrightness.pokemonImageBrightnessFinder(folderPath, fileName)) + multipleTypes.title() + '\n')
-    with open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Log.txt', 'a') as infoLog:
+    with open('/home/adam/Pokemon_Images/Pokemon_Compiled_Info_Log_Temp.txt', 'a') as infoLog:
         infoLog.write(fileName + '\n')
